94a8f2ab294797dba4294eea6386ada0984d687a
Tauhid hossen <tauhidhossen04@gmail.com>
2025-06-17 08:38:08 +0600

    Fixed the mistake of some queries

--- assignment.sql
+++ assignment.sql
@@ -1,55 +1,57 @@
 -- Active: 1747481365322@@127.0.0.1@5432@conservation_db
+DROP TABLE IF EXISTS sightings;
+DROP TABLE IF EXISTS species;
+DROP TABLE IF EXISTS rangers;
+
 
 --create a ranger table
-create table ranger (
+CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) not null,
-    region varchar(50) not null
-)
+    name VARCHAR(50) NOT NULL,
+    region VARCHAR(50) NOT NULL
+);
 
 --create species table
 
-CREATE table  species (
-    species_id serial PRIMARY key,
-    common_name VARCHAR(50) not null,
-    scientific_name varchar(50) not null,
-    discovery_date DATE not NULL,
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(50) NOT NULL,
+    discovery_date DATE NOT NULL,
     conservation_status VARCHAR(50) CHECK (
-        conservation_status IN ('Endangered','Vulnerable', 'Historic')
+        conservation_status IN ('Endangered', 'Vulnerable', 'Historic')
     )
-
-)
+);
 
 
 
 -- crate sightings table
-CREATE table sightings (
-    sighting_id serial primary key,
-    species_id int REFERENCES species(species_id),
-    ranger_id int REFERENCES ranger(ranger_id),
-    sighting_time TIMESTAMP not null,
-    locations VARCHAR(100) not null,
-    note text
-
-)
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species(species_id),
+    ranger_id INT REFERENCES rangers(ranger_id),
+    sighting_time TIMESTAMP NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    notes TEXT
+);
 
 
 --insert data into ranger table
-insert INTO ranger (name,region) VALUES 
+INSERT INTO rangers (name, region) VALUES
 ('Alice Green', 'Northern Hills'),
-('Bob white', 'River Delta'),
-('coral king', 'mountain Range');
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range');
 
 --check ranger table
 select * from ranger
 
 --insert data into species table
 
-insert into species (common_name, scientific_name, discovery_date,conservation_status) VALUES 
-('snow leopard','Panther uncia','1775-01-01','Endangered' ),
-('Bengal Tiger','Panther tigris tigris','1758-01-01','Endangered' ),
-('Red Panda','Ailurus fulgens','1825-01-01','Vulnerable' ),
-('Asiatic Elephant','Elephas maximus indicus','1758-01-01','Endangered' );
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
 --check species table
 select * from species
@@ -57,48 +59,52 @@ select * from species
 
 -- insert data into sightings table
 
-insert into sightings (sighting_id, species_id, ranger_id, locations,sighting_time,note) VALUES
-(1,1,1, 'peak ridge', '2024-05-10 07:45:00', 'camera trap image captured'),
-(2,2,2, 'Backwood area', '2024-05-12 16:20:00', 'juvenile seen'),
-(3,3,3, 'Bamboo grove East', '2024-05-15 09:10:00', 'Feeding observed'),
-(4,1,2, 'Snowfall pass', '2024-05-18 18:30:00', null);
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
 
 SELECT * from sightings
 
 --Q1: register a new ranger
-insert into ranger (name, region) VALUES('Derek Fox', 'Costal Plains');
+INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
+
 
 --Q2: Count unique species ever sighted
-SELECT count(DISTINCT species_id) as unique_species_counts
-from sightings
+SELECT COUNT(DISTINCT species_id) AS unique_species_count FROM sightings;
+
 
 --Q3: find sighting where location includes "pass"
+SELECT * FROM sightings
+WHERE location ILIKE '%Pass%';
 
-select * from sightings
-where locations ILIKE '%pass%';
+
 
 --Q4: list each anger names and their total number of sighting
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r
+LEFT JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY r.name
+ORDER BY r.name;
 
-select ranger.name, count(sightings.sighting_id) from ranger
-inner join sightings on ranger.ranger_id = sightings.ranger_id
-group by ranger.name  
-order by ranger.name;
+
 
 
 --Q5:list species that have never been sighted
-
 select common_name from species
 where species_id not in 
 (select DISTINCT species_id from sightings);
 
 
 --Q6: show the most recent 2 sightings
-
-select sp.common_name, s.sighting_time, r.name from sightings as s
-join species as sp on s.species_id = sp.species_id
-join ranger as r on s.ranger_id = r.ranger_id
-order by s.sighting_time  DESC
+SELECT sp.common_name, si.sighting_time, r.name
+FROM sightings si
+JOIN species sp ON si.species_id = sp.species_id
+JOIN rangers r ON si.ranger_id = r.ranger_id
+ORDER BY si.sighting_time DESC
 LIMIT 2;
+
 
 
 --Q7: update all species discovered before 1800 status 'Historic'
@@ -120,7 +126,9 @@ order by sighting_id;
 
 --Q9:delete rangers who have never sighted any species
 
-delete from ranger
+delete from rangers
 where ranger_id not in (select DISTINCT ranger_id from sightings);
+
+
 
 
